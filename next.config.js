51086b6e8b29f12ea18453c0f9122de3a6128dfa
youngmoedward <youngmo.edward.yang@gmail.com>
2026-07-25 20:05:41 -0700

    fix: fixed build errer again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracing: false,
   webpack: (config, { dev }) => {
     // 개발 모드일 때 Windows 디스크 파일 락(EPERM) 및 CSS 청크 꼬임 해결을 위해 웹팩 캐시 타입을 메모리로 전환
     if (dev) {
